58679d923524043138b41154aebf29f8d0a8c2f5
steckdoose4711 <62461086+Steckdoose4711@users.noreply.github.com>
2020-10-22 12:24:17 +0200

    added csv generation and gettimeofday-measurement

--- Roundtrip_Measurement/src/udp.cpp
+++ Roundtrip_Measurement/src/udp.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <chrono>
 #include <boost/asio.hpp>
+#include <sys/time.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
 
 #include "latency.hpp"
 
@@ -21,22 +25,49 @@ int ping_udp(const std::string& host, size_t count)
 
     auto clock = std::chrono::high_resolution_clock{};
 
+    std::vector<__suseconds_t> times_measured;
+
     for(size_t i = 0; i < count; i++)
     {
         auto now = clock.now();
         bool warm = i >= count/2;
+
+        timeval tim_send;
+        timeval tim_rec;
+
+        gettimeofday(&tim_send, NULL);
 
         socket.async_send_to(boost::asio::buffer(data, 512), sender_endpoint,
                              [&](const boost::system::error_code &error, size_t bytes_recvd)
                              {
                                  socket.async_receive_from(boost::asio::buffer(data, 512), dummy, [&](const boost::system::error_code &error, size_t bytes_recvd) {
                                      if(warm)
+                                     {
                                         l.add(clock.now() - now);
+                                        gettimeofday(&tim_rec, NULL);
+                                        times_measured.emplace_back(tim_rec.tv_usec - tim_send.tv_usec);
+                                    }
                                  });
                              });
 
         io_service.run();
         io_service.reset();
+    }
+
+    try
+    {
+        std::ofstream myfile;
+        myfile.open ("durations.csv");
+        for(size_t i = 0; i < times_measured.size(); i++)
+        {
+            myfile << times_measured[i] << ",";
+        }
+
+        myfile.close();
+    }
+    catch(...)
+    {
+        std::cerr << "Error writing to csv file" << std::endl;
     }
 
     l.generate(std::cout);
