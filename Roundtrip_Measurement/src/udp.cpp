a07d0c4eb56b7b91b3b83d3fb4776c826fc8818e
steckdoose4711 <62461086+Steckdoose4711@users.noreply.github.com>
2020-10-25 00:12:49 +0200

    added debug information for testing

--- Roundtrip_Measurement/src/udp.cpp
+++ Roundtrip_Measurement/src/udp.cpp
@@ -5,35 +5,41 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
-
 #include "latency.hpp"
 
 using boost::asio::ip::udp;
 
-static size_t const size_payload_byte = 2048;
+static size_t const size_payload_byte = 256;
 
 int ping_udp(const std::string& host, size_t count)
 {
     std::cout << "Ping " << host << " * " << count << " ..." << std::endl;
 
-    //auto l = latency<(std::uint64_t)1 * 1000 * 1000 * 1000>();
-
     boost::asio::io_service io_service;
     udp::socket socket{io_service, udp::endpoint(udp::v4(), 20123)};
 
     char data[size_payload_byte];
-    udp::endpoint sender_endpoint = *(udp::resolver{io_service}.resolve(udp::resolver::query(host, "20124")));
+    udp::endpoint sender_endpoint = *(udp::resolver{io_service}.resolve(udp::resolver::query(host, "10000")));
     udp::endpoint dummy;
+    size_t percentage_cnt = count /100;
+    size_t percentage_print = 1;
 
     std::vector<__suseconds_t> times_measured;
 
     for(size_t i = 0; i < count; i++)
     {
-        bool warm = i >= count/2;
+        //bool warm = i >= count/2;
 
         timeval tim_send;
         timeval tim_rec;
         float elapsedTime;
+
+        if(i > percentage_cnt)
+        {
+            std::cout << percentage_print << "%" << std::endl;
+            percentage_print += 1;
+            percentage_cnt += count / 100;
+        }
 
         gettimeofday(&tim_send, NULL);
 
@@ -41,14 +47,14 @@ int ping_udp(const std::string& host, size_t count)
                              [&](const boost::system::error_code &error, size_t bytes_recvd)
                              {
                                  socket.async_receive_from(boost::asio::buffer(data, size_payload_byte), dummy, [&](const boost::system::error_code &error, size_t bytes_recvd) {
-                                     if(warm)
+                                     //if(warm)
                                      {
                                         gettimeofday(&tim_rec, NULL);
 
                                         elapsedTime = (tim_rec.tv_sec - tim_send.tv_sec) * 1000000.0;      // sec to us
                                         elapsedTime += (tim_rec.tv_usec - tim_send.tv_usec);
                                         times_measured.push_back(elapsedTime);
-
+                                        std::cout i << ": " << elapsedTime << "us" << std::endl;
                                     }
                                  });
                              });
@@ -82,7 +88,7 @@ int pong_udp(const std::string& host)
     std::cout << "Pong " << host << " ..." << std::endl;
 
     boost::asio::io_service io_service;
-    udp::socket socket{io_service, udp::endpoint(udp::v4(), 20124)};
+    udp::socket socket{io_service, udp::endpoint(udp::v4(), 10000)};
 
     char data[512];
     udp::endpoint sender_endpoint;
